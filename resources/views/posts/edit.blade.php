1eecd3d5caf915d8fedfa3c14779ff2efe13a213
ahmedaljendy <elgendya166@gmail.com>
2025-03-10 01:48:46 +0200

    fix formatting in post edit view by adding a new line for better readability

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -31,6 +31,7 @@
                             class="w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50 py-2 px-3 border"
                         
                         > {{ $post->description }}</textarea>
+                        
                     </div>
                     {{-- @dd($users); --}}
                     <!-- Post Creator Select -->
