6d874780362dc561e25dd9777fd3d38b2ee7c93c
ahmedaljendy <elgendya166@gmail.com>
2025-03-10 01:48:42 +0200

    add destroy method to CommentController for deleting comments

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Comment;
+use App\Models\Post;
+
 class CommentController extends Controller
 {
     public function store(Request $request, $postId)
@@ -18,4 +20,14 @@ class CommentController extends Controller
 
         return redirect()->back()->with('success', 'Comment added!');
     }
+    public function destroy($id)
+    {
+        $comment = Comment::find($id);
+        $comment->delete();
+        // dd($id);
+        return redirect()->back()->with('success', 'Comment deleted!');
+    }
+    
+    
+
 }
