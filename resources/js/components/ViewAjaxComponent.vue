bc7f00f7bc23e6c523404ee125bdd4807cfbdb9a
ahmedaljendy <elgendya166@gmail.com>
2025-03-12 14:57:00 +0200

    add ViewAjaxComponent for displaying details with a button

---
+++ resources/js/components/ViewAjaxComponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="inline-block">
+        <button
+            class="px-4 py-2 bg-blue-600 text-white font-medium rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 transition duration-200 ease-in-out"
+        >
+            View Details
+        </button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {};
+    },
+    props: ["id"],
+};
+</script>
+
+<style></style>
