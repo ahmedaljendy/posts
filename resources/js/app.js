5689934eb20def2af74f27380df97347f418f75b
ahmedaljendy <elgendya166@gmail.com>
2025-03-12 14:57:08 +0200

    add Vue app initialization and register ViewAjaxComponent

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,6 @@
-import './bootstrap';
+import "./bootstrap";
+
+import { createApp } from "vue";
+import IncrementCounter from "./components/IncrementCounter.vue";
+import ViewAjaxComponent from "./components/ViewAjaxComponent.vue";
+createApp({}).component("ViewAjax", ViewAjaxComponent).mount("#app");
