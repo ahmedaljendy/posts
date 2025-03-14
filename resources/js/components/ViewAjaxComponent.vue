57c76b1226c5e956b14fc104dcd82e6d4a501379
ahmedaljendy <elgendya166@gmail.com>
2025-03-15 01:43:10 +0200

    add fetchAndOpenModal method to ViewAjaxComponent for fetching post details

--- resources/js/components/ViewAjaxComponent.vue
+++ resources/js/components/ViewAjaxComponent.vue
@@ -2,6 +2,7 @@
     <div class="inline-block">
         <button
             class="px-4 py-2 bg-blue-600 text-white font-medium rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 transition duration-200 ease-in-out"
+            @click="fetchAndOpenModal"
         >
             View Details
         </button>
@@ -9,9 +10,27 @@
 </template>
 
 <script>
+import { useModalStore } from "../store/posts.js";
+
 export default {
     data() {
-        return {};
+        return {
+            modalStore: useModalStore(),
+            isModalOpen: false,
+        };
+    },
+
+    methods: {
+        async fetchAndOpenModal() {
+            try {
+                const response = await axios.get(
+                    `http://localhost:8000/api/posts/${this.id}`
+                );
+                this.modalStore.openModal(response.data);
+            } catch (error) {
+                console.error("Error fetching post:", error);
+            }
+        },
     },
     props: ["id"],
 };
