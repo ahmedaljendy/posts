cc6ba94af511966830635e586c58f06e53606875
ahmedaljendy <elgendya166@gmail.com>
2025-03-15 01:42:25 +0200

    add Pinia state management and Axios integration in Vue app

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,21 @@
 import "./bootstrap";
 
 import { createApp } from "vue";
-import IncrementCounter from "./components/IncrementCounter.vue";
+import { createPinia } from "pinia";
 import ViewAjaxComponent from "./components/ViewAjaxComponent.vue";
-createApp({}).component("ViewAjax", ViewAjaxComponent).mount("#app");
+import axios from "axios";
+import ModalComponent from "./components/ModalComponent.vue";
+
+// Set Axios defaults (optional)
+axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
+
+const app = createApp({});
+const pinia = createPinia();
+
+app.component("view-ajax", ViewAjaxComponent);
+app.component("post-modal", ModalComponent);
+
+app.use(pinia);
+app.mount("#app");
+
+window.axios = axios;
